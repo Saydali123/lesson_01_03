9878344ddf9e910e579f652643b2298bfde871d8
Jonibek WA <jonee.av@gmail.com>
2022-03-02 13:00:13 +0500

    DB

--- src/main/java/uz/elmurodov/lesson_01_03/db/DataBase.java
+++ src/main/java/uz/elmurodov/lesson_01_03/db/DataBase.java
@@ -2,6 +2,7 @@ package uz.elmurodov.lesson_01_03.db;
 
 import uz.elmurodov.lesson_01_03.models.Group;
 import uz.elmurodov.lesson_01_03.models.Student;
+import uz.elmurodov.lesson_01_03.models.Subject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.List;
 public class DataBase {
     public static final List<Student> STUDENTS = new ArrayList<>();
     public static final List<Group> GROUPS = new ArrayList<>();
+    public static final List<Subject> SUBJECTS = new ArrayList<>();
 }
